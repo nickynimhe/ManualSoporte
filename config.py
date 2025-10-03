1c89632ff96eccf8397f9fee7fdf30533dd43195
JarithHernandez <167656465+nickynimhe@users.noreply.github.com>
2025-10-03 14:38:20 -0500

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'clave-super-secreta-para-desarrollo'
     
     # Configuración de MySQL
-    MYSQL_HOST = os.environ.get('MYSQL_HOST') or 'localhost'
-    MYSQL_USER = os.environ.get('MYSQL_USER') or 'root'
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD') or ''
-    MYSQL_DB = os.environ.get('MYSQL_DB') or 'soporte_tecnico'
+    MYSQL_HOST = os.environ.get('dpg-d3g1q2nfte5s73cjpiu0-a') or 'dpg-d3g1q2nfte5s73cjpiu0-a'
+    MYSQL_USER = os.environ.get('soporte_tecnico_9sad_user') or 'soporte_tecnico_9sad_user'
+    MYSQL_PASSWORD = os.environ.get('T56GYS3Oj5w4kGzrdlvAhlGfExjT0t7a') or 'T56GYS3Oj5w4kGzrdlvAhlGfExjT0t7a'
+    MYSQL_DB = os.environ.get('soporte_tecnico_9sad') or 'soporte_tecnico_9sad'
     
     # Configuración adicional
     DEBUG = os.environ.get('FLASK_DEBUG', 'False').lower() in ('true', '1', 't')
